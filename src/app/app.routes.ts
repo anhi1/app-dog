700f63458afe6c6f19b0759998401c77491411da
anhi1 <anhiemolina@gmail.com>
2025-05-02 23:02:17 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 export const routes: Routes = [
-    { path: '', component: CardComponent }
+    { path: '', component: CardComponent },
+    { path: 'contacto', component: ContactoComponent}
 ];
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatButtonModule, MatMenuModule],
+  imports: [MatButtonModule, MatMenuModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
